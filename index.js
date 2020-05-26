534f9f288d839a2eae2be8ccacdbcaf0c8119793
Serhii Yakushov <serhii.yakushov@right-now-group.de>
2020-05-26 10:03:36 +0300

    second commit

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 require('dotenv').config()
 const { retry, retries } = require('./retry');
-const Table = require('./table/visitors');
+const Table = require('./table');
 const request = require('./request');
 
 
 const loginPath = `${process.env.HOST}/api/login`;
 const dataPath = (pageNo = 1, token) => `${process.env.HOST}/api/visits?page=${pageNo}&token=${token}`;
+const MAX_LENGTH = 15;
 
 
-const getData = (pageNo, token) => request(dataPath(pageNo, token))
+const getData = async (pageNo, token) => request(dataPath(pageNo, token));
 
+const loop = async (startFromPage, pageCount, token, visitorsBatch = []) => {
+  const promises = [];
+  for (let page = startFromPage; page <= pageCount; page++) {
+    promises.push(() => getData(page, token))
+  }
+
+  (await retries(promises)).forEach(visitor => visitorsBatch.push(visitor));
+  
+  const maxTotalCount = visitorsBatch.reduce((prev, current) => (prev.total > current.total) ? prev.total : current.total);
+  const maxPageCount = Math.ceil(maxTotalCount / MAX_LENGTH);
+
+  if (maxPageCount > pageCount) {
+    await loop(pageCount + 1, maxPageCount, token, visitorsBatch)
+  }
+  
+  return visitorsBatch;
+}
 
 const runScript = async () => {
-  const promises = []
   const storage = new Table();
 
   const token = await retry(() => request(loginPath)).then(({ token }) => token);
   const visitorsData = await getData(1, token);
-  storage.setAll(visitorsData.data);
+  const pageCount = Math.ceil(visitorsData.total / MAX_LENGTH);
 
-  const pageCount = Math.ceil(visitorsData.total / visitorsData.data.length);
-
+  const visitorRecords = await loop(1 , pageCount, token, []);
+  visitorRecords.forEach(({data}) => storage.setAll(data));
   
-  if ( pageCount > 1 ) {
-    for (let page = 2; page <= pageCount; page++) {
-      promises.push(() => getData(page, token))
-    }
-    await retries(promises)
-      .then((visitors => visitors.forEach(({data}) => storage.setAll(data))));
-  }
-  
-  console.log('token', token);
-  console.log('pageCount', pageCount);
   console.log('storage', storage.getData());
-  
+  console.log('visitorRecordsBatchNo', visitorRecords.length);
   return storage.getData();
 }
 
--- table/index.js
+++ table/index.js
@@ -1,15 +1,26 @@
 class Index {
   constructor() {
     this.data = {};
-    this.currentDate = new Date(new Date().toJSON().slice(0,10));
+    this.currentDate = new Date(new Date().toJSON().slice(0,10)); // begin of current day
+    this.weekDays = [1,2,3,4,5]; // 1 - mon, 2 - tue, 3 - wen, 4 - thu, 5 - fri
   }
-  set(name) {
-    if (this.data[name]) {
-      this.data[name] += 1;
-    } else {
-      this.data[name] = 1;
-    }
+  set(visitor) {
+    const { date, name } = visitor;
+    const givenDate = new Date(date);
+    const isWeekDays = this.weekDays.includes(givenDate.getDay());
 
+    if(this.currentDate > givenDate && isWeekDays){
+      if (this.data[name]) {
+        this.data[name] += 1;
+      } else {
+        this.data[name] = 1;
+      }
+    }
+    
+    return this.data;
+  }
+  setAll(visitors) {
+    visitors.forEach(visitor => this.set(visitor));
     return this.data;
   }
   get(key) {
